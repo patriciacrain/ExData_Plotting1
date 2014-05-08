13e5d7b9e6a0baa52a571b4de9ea05312f86cbcc
Patricia Coates <patriciajcrain@gmail.com>
2014-05-07 22:20:04 -0300

    A minor change to plot 4

--- plot4.R
+++ plot4.R
@@ -15,7 +15,7 @@ with(household, {
   plot(Time,Sub_metering_1, type="l", xlab="", ylab="Energy sub metering" )
   lines(Time,Sub_metering_2, type="l", col="red")
   lines(Time,Sub_metering_3, type="l", col="blue")
-  legend("topright", lty=1, col = c("black","blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  legend("topright", lty=1, col = c("black","blue", "red"), bty="n", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Time, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 })
 dev.off()
